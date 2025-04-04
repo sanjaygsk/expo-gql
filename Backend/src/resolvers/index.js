ca7446ac81b6d55fc7a1af67147509dc3a783dd9
Sanjay Kumar G G <sanjaykumar.g@zohocorp.com>
2025-04-04 13:51:33 +0530

    {FIX} - Updated graphql type json in resolver

--- Backend/src/resolvers/index.js
+++ Backend/src/resolvers/index.js
@@ -1,13 +1,15 @@
 import { Query } from "./Query.js";
 import { Mutation } from "./Mutation.js";
+import GraphQLJSON from "graphql-type-json"
 
 // Import your models here to use in the resolvers
 
 
 const resolvers = {
+    JSON: GraphQLJSON,
     // All Get Operations
     Query: {
-        
+
     },
 
     // All Post Operations
